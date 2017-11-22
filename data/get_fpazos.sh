137bceb90c2c3ee076eb5ba1526f2393d2a6454f
Ignacio Ramirez <nacho@fing.edu.uy>
2017-11-22 15:49:25 -0300

    Added more datasets: human genome and genome functions

---
+++ data/get_fpazos.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -c http://iie.fing.edu.uy/~nacho/data/fpazos-funciones_de_genes.7z
+7zr x fpazos-funciones_de_genes.7z
---
+++ data/get_hgdp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -c http://iie.fing.edu.uy/~nacho/data/human_genome.7z
+7zr x human_genome.7z
