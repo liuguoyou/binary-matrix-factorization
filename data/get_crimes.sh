ef9fa9c5bed46d58d7c5bd6b498afeb267241aed
Ignacio Ramirez <nacho@fing.edu.uy>
2017-11-23 15:29:55 -0300

    added crimes dataset

---
+++ data/get_crimes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Downloaded from Stanford GEnotypes project
+# http://www.hagsc.org/hgdp/files.html
+#
+# This paper explains how to perform PCA on this dataset: http://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.0020190
+#
+#
+# this is the small matrix; there are WAY larger ones out there
+wget -c http://www.hagsc.org/hgdp/data/crimenes_2017_09.zip
