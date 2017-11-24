f127a16aafbb661620a38bdf1b65d96d9ddfeead
Ignacio Ramirez <nacho@fing.edu.uy>
2017-11-23 21:02:30 -0300

    Working on hgdp parser.

--- data/parse_hgdp.c
+++ data/parse_hgdp.c
@@ -34,9 +34,12 @@ int main(int argc, char* argv[]) {
   FILE* fmask = NULL;
 
   char* line = NULL;
-  size_t m,n = 0;
+  char* tmp;
+  size_t len;
+  size_t m = 0,n = 0;
   ssize_t r;
-
+  char* tok;
+  char* modes;
   unsigned long Af = 0;
   unsigned long Tf = 0;
   unsigned long Gf = 0;
@@ -49,7 +52,7 @@ int main(int argc, char* argv[]) {
   //
   // 1 scan number of rows and columns in file
   //
-  r = getline(&line,&n,fin); // skip header
+  r = getline(&line,&len,fin); // skip header
   //
   // 1.1 scan number of columns from header
   tok = strtok(line," \n\t\r");
@@ -58,90 +61,265 @@ int main(int argc, char* argv[]) {
     tok = strtok(NULL," \n\t\r");
   }
   if (n == 0) {
-    fprintf(stderr,"Empty or invalid file: %s\n,"argv[1]);
+    fprintf(stderr,"Empty or invalid file: %s\n",argv[1]);
     exit(5);
   }
+  printf("Number of columns: %lu\n",n);
   //
-  // 2.1 scan number of rows and compute mode
+  // 1.2 scan number of rows
   //
-  fmode = fopen("mode.pbm","w");
-  if (!mode) return 2;
-  while ((r = getline(&line,&n,fin)) > 0) {
+  while ((r = getline(&line,&len,fin)) > 0) {
     m++;
+  }
+  printf("Number of rows: %lu\n",m);
+  //
+  // compute mode
+  // 
+  fmode = fopen("mode.pbm","w");
+  fprintf(fmode,"P4 1 %lu\n",m); 
+  if (!fmode)
+    return 2;
+  modes = (char*) malloc(m*sizeof(char));
+
+  printf("SECOND PASS.\n");
+  rewind(fin);
+  r = getline(&line,&len,fin); // skip header
+  size_t i = 0;
+  int c = 0;
+  while ((r = getline(&line,&len,fin)) > 0) {
+    size_t ni = 0; 
     tok = strtok(line," \n\t\r"); // skip gene name
     Af = Tf = Gf = Cf = 0;
-    tok = strtok(line," \n\t\r");
+    tok = strtok(NULL," \n\t\r");
     while (tok) {
-      switch (*tok)  { // first letter
+      ni++;
+      switch (tok[0])  { // first letter
       case 'A':
-	atgc_f
+	Af++;
 	break;
       case 'T':
+	Tf++;
 	break;
       case 'G':
+	Gf++;
 	break;
       case 'C':
+	Cf++;
 	break;
       case '-':
 	break;
       }
+      switch (tok[1])  { // first letter
+      case 'A':
+	Af++;
+	break;
+      case 'T':
+	Tf++;
+	break;
+      case 'G':
+	Gf++;
+	break;
+      case 'C':
+	Cf++;
+	break;
+      case '-':
+	break;
+      }
+      //
+      // next 
+      //
       tok = strtok(NULL," \n\t\r");
-      if (tok) nr++;
     }
-  }
-  if (m == 0) {
-    fprintf(stderr,"Empty or invalid file: %s\n,"argv[1]);
-    exit(6);
-  }
-  //
-  // go back to the top, compute the mode
-  //
-  printf("Input file has %d rows and %d columns.\n",m,n);
-  rewind(fin);
-  
-  f = fopen("dist.pbm","w");
-  if (!mode) return 3;	 
-  fmode = fopen("mask.pbm","w");
-  if (!mode) return 4;
-  
-  // copy header as is
-  // it is also the longest line, so it will suffice for latter calls
-  r = getline(&line,&n,fin);
-  fputs(line,fout);  
-  // now parse each line
-  size_t n = 0; // number of columns in data file
-  size_t nri = 0; // number of columns in row i, for control
-  size_t m = 0; // number of rows
-  while (getline(&line,&n,fin) > 0) {
-    char *tok;
-    nr = 0;
-    tok = strtok(line," \n\t\r"); // skip gene name
-    tok = strtok(line," \n\t\r");
-    while (tok) {
-      fprintf(fout,"%s|",tok);
-      printf("%s|",tok);
-      tok = strtok(NULL," \n\t\r");
-      if (tok) nr++;
+    if (ni != n) {
+      fprintf(stderr,"Wrong number of colums (%lu should be %lu) at line %lu\n",ni,n,m);
+      exit(7);
     }
-    if (nr1 == 0) {
-      nr1 = nr;
-      printf("Num
-    } else {
-      if (__1 != nr) {
-        fprintf(stderr,"ERROR: wrong number of columns at line %lu\n",ln);
-        fclose(fin);
-        fclose(fout);
-        free(line);
-        return 3;
+    //
+    // compute and output mode to mode.pbm
+    // A,T,G,C
+    char mode='A';
+    size_t Mf = Af;
+    if (Tf > Mf) {
+      mode='T';
+      Mf = Tf;
+    }
+    if (Gf > Mf) {
+      mode='G';
+      Mf = Gf;
+    }
+    if (Cf > Mf) {
+      mode='C';
+      Mf = Cf;
+    }
+    modes[i] = mode;
+    //
+    // write two nibbles to  mode.pbm
+    //
+    if (i % 2) {
+      c <<= 4;
+      switch (mode) {
+      case 'A':
+	c |= 0x01;
+	break;
+      case 'T':
+	c |= 0x02;
+ 	break;
+      case 'G':
+	c |= 0x04;
+	break;
+      case 'C':
+	c |= 0x08;
+	break;
+      }
+      fputc(c,fmode);
+      c = 0;
+    } else {  
+      switch (mode) {
+      case 'A':
+	c = 0x01;
+	break;
+      case 'T':
+	c = 0x02;
+	break;
+      case 'G':
+	c = 0x04;
+	break;
+      case 'C':
+	c = 0x08;
+	break;
       }
     }
-    ln++;
-    fputc('\n',fout);
-    printf("\n");
+    i++;
+  } // end second pass
+  fclose(fmode);
+  if (m == 0) {
+    fprintf(stderr,"Empty or invalid file: %s\n",argv[1]);
+    exit(6);
   }
+  printf("THIRD PASS.\n");
+
+  tmp = (char*)malloc(n*sizeof(char));
+  //
+  // third pass: generate dist and mask
+  //
+  printf("Input file has %lu rows and %lu columns.\n",m,n);
+  
+  fdist = fopen("dist.pbm","w");
+  if (!fdist) return 3;	 
+  fmask = fopen("mask.pbm","w");
+  if (!fmask) return 4;
+  fprintf(fdist,"P4 %lu %lu 8\n",2*m,n);
+  fprintf(fmask,"P4 %lu %lu\n",2*m,n);
+  // now parse each line; write to fdist and fmask
+  //
+  i = 0;  
+  rewind(fin);
+  r = getline(&line,&len,fin); // skip header
+  while (getline(&line,&len,fin) > 0) {
+    tok = strtok(line," \n\t\r"); // skip gene name
+    tok = strtok(line," \n\t\r");
+    size_t j = 0;
+    char cd = 0, cm = 0;
+    while (tok) {
+      const char a = tok[0];
+      const char b = tok[1];
+      const char mode = modes[i];
+      if (a == '-') {
+	tmp[j++]='-';
+      } else {
+	if (a == mode) {
+	  if (b == mode) {
+	    tmp[j++] = '0';
+	  } else {
+	    tmp[j++] = '1';
+	  }
+	} else { // a != mode
+	  if (b == mode) {
+	    tmp[j++] = '1';
+	  } else {
+	    tmp[j++] = '2';
+	  }
+	}
+      }
+      tok = strtok(NULL," \n\t\r");
+    }
+    tmp[j] = 0;
+    puts(line);
+    // now we've got a string of length n with '-','0','1' or '2'
+    // we traverse it twice, and write TWO lines  on mask and dist
+    // the first row is the 'least significant row'
+    // and can only be '1' if dist='1'
+    char mask = 0x80;
+    for (j = 0; j < n; j++) {      
+      switch (tmp[j]) {
+      case '-': // 0
+	break;
+      case '0': // 0 above, 0 below
+	cm |= mask;
+	break;  
+      case '1': // 
+	cd |= mask;
+	cm |= mask;
+	break;
+      case '2':
+	cm |= mask;
+	break;
+      }
+      mask >>= 1;
+      if (!mask) {
+	fputc(cd,fdist);
+	fputc(cm,fmask);
+	mask = 0x80;
+      }
+    }
+    if (mask) { // did not finish byte
+      while (mask) {
+	cd <<= 1;
+	cm <<= 1;
+      }
+      fputc(cd,fdist);
+      fputc(cm,fmask);      
+    }
+    // second 'most significant' row, can only be '1' if dist='2'
+    for (j = 0; j < n; j++) {      
+      switch (tmp[j]) {
+      case '-': // 0
+	break;
+      case '0': // 0 above, 0 below
+	cm |= mask;
+	break;  
+      case '1': // 
+	cm |= mask;
+	break;
+      case '2':
+	cd |= mask;
+	cm |= mask;
+	break;
+      }
+      mask >>= 1;
+      if (!mask) {
+	fputc(cd,fdist);
+	fputc(cm,fmask);
+	mask = 0x80;
+      }
+    }
+    if (mask) { // did not finish byte
+      while (mask) {
+	cd <<= 1;
+	cm <<= 1;
+      }
+      fputc(cd,fdist);
+      fputc(cm,fmask);      
+    } 
+    i++;
+    fputc('\n',fmask);
+    fputc('\n',fdist);
+  } // third pass loop
+  free(modes);
   fclose(fin);
-  fclose(fout);
+  fclose(fmask);
+  fclose(fdist);
   free(line);
   return 0;
 }
-
+  
